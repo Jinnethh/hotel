5da96d31c27df93f6632408f91b93420e13704cb
Jinnethh <160985872+Jinnethh@users.noreply.github.com>
2024-06-27 16:43:40 -0500

    Update bienvenida.vue

--- src/components/bienvenida.vue
+++ src/components/bienvenida.vue
@@ -1,100 +1,62 @@
 <template>
-  <div class="hola">
+   <div class="con">
+    <div class="image-section">
+        <img src="https://images.ecestaticos.com/-znn8nLSacjksMxwSsIk9moXomA=/0x0:1639x827/1338x675/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F21a%2F98f%2Ffe7%2F21a98ffe722e93e62773b10cc1df4688.jpg">
+    </div>
+    <div class="text-section">
+        <h1>Bienvenido al Hotel  Crystal House</h1>
+        <p> la comodidad y la elegancia se unen para brindarte una experiencia inolvidable. Descubre nuestras amplias habitaciones, exquisita gastronomía y servicios de primera clase. ¡Tu escapada perfecta comienza aquí!"</p>
+    </div>
+</div>
 
-    <div class="contenedor">
-      <img src="./vista-borrosa-restaurante-vacio_1203-1052.jpg" alt="">
-
+<div class="destacados">
+    <h2>Destacados</h2>
+    <div class="highlight-item">
+      <img src="./piscina.png" alt="">
+        <h4>Piscinas</h4>
+       <hr>
     </div>
 
-    <div class="cuadros">
-      <div id="partes">
-        <img
-          src="https://www.hotelbilbaoplaza.com/uploads/hotelbilbaoplaza/8127/hotel-bilbao-plaza-king-bed-vista-panoramica.jpg?0"
-          alt="">
-      </div>
-      <div id="partes">
-        <img
-          src="https://images.pexels.com/photos/7258844/pexels-photo-7258844.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="">
-      </div>
-      <div id="partes">
-        <img src="https://as2.ftcdn.net/v2/jpg/05/45/74/31/1000_F_545743151_yNarKEpeBs1uMPnw8FdbDxrvPqa7Ch2O.jpg"
-          alt="">
-      </div>
-
+    <div class="highlight-item">
+      <img src="./bandeja.png" alt="">
+        <h4>Restaurantes</h4>
+        <hr>
     </div>
 
-    <div class="introduccion">
-      <h2>"Bienvenido al Hotel Oasis, donde la comodidad y
-        la elegancia se unen para brindarte una experiencia inolvidable.para satisfacer todas tus necesidades.
-        Descubre nuestras amplias habitaciones, exquisita gastronomía y servicios de primera clase. ¡Tu escapada
-        perfecta comienza aquí!"</h2>
+    <div class="highlight-item">
+      <img src="./spa.png" alt="">
+        <h4>Spas</h4>
+        <hr>
+    </div>
+
+    <div class="highlight-item">
+      <img src="./relajacion.png" alt="">
+        <h4>Zona de Relajación</h4>
+        <hr>
+      </div>    
+</div>
+
+<div class="collage">
+  <div class="piscina">
+    <img src="https://greenarea.es/wp-content/uploads/2017/11/restaurante-jardin-vertical-hotel-llaut-palace-greenarea-decoracion.jpg" alt="Piscina 1">
   </div>
-
+  <div class="piscina">
+    <img src="https://coolpool.es/wp-content/uploads/2022/09/restaurantes-con-piscinas-purobeach.jpg" alt="Piscina 2">
   </div>
-  <div class="segundaparte">
-    <section id="slideshow">
-      <div class="entire-content">
-        <div class="content-carrousel">
-          <figure class="shadow"><img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCIW3KAgvgVgYj8VIpvWhUX6-i5zdG66qqZQ&s" />
-          </figure>
-          <figure class="shadow"><img src="https://wallpaperswide.com/download/hotel_room_2-wallpaper-3554x1999.jpg" />
-          </figure>
-          <figure class="shadow"><img
-              src="https://www.estelarparquedela93.com/cache/42/05/42053e53569c891c4c412baa3c5b2884.jpg" /></figure>
-          <figure class="shadow"><img
-              src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/54259453.jpg?k=5564c1363e1472719ba7f63864fd7e9c5778dce2e61237ee4a98330be9c7fb2a&o=&hp=1" />
-          </figure>
-          <figure class="shadow"><img
-              src="https://media-cdn.tripadvisor.com/media/photo-s/07/1d/7d/78/regency-country-club.jpg" /></figure>
-          <figure class="shadow"><img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR56GUXY_-DiLVuOyMxs8pb01ci5n_U_SNpBA&s" />
-          </figure>
-          <figure class="shadow"><img src="https://i.blogs.es/b9d2f5/foto-apertura-haroma-heritage/450_1000.jpg" />
-          </figure>
-
-          <figure class="shadow"><img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9f502KLDDvighagzHaCUDe-8M8LWzgialhw&s" />
-          </figure>
-
-          <figure class="shadow"><img
-              src="https://i.pinimg.com/originals/a1/ae/29/a1ae296603e2ce009a17cc4e4a326c32.jpg" /></figure>
-
-
-        </div>
-      </div>
-    </section>
-
-
-
-    <div class="destacados">
-      <h2>Destacados</h2>
-      <h4> Pscinas</h4>
-      <p>El hotel cuenta con una impresionante piscina al aire libre con vistas panorámicas, perfecta para disfrutar
-        del
-        sol y relajarse. Ambas piscinas están rodeadas de cómodas tumbonas y cabañas privadas, ofreciendo un servicio
-        de
-        bar para refrescarse con cócteles y aperitivos.</p>
-      <h4>Restaurantes</h4>
-      <p>
-        Restaurantes
-        Dos restaurantes destacados: uno con cocina internacional gourmet y otro de especialidades con menús
-        degustación. También hay un café para desayunos y un bar en la azotea para tapas y bebidas.</p>
-    </div>
-    <div class="destacado">
-      <h4>Spas</h4>
-      <p>El spa ofrece masajes, tratamientos faciales, saunas, baños de vapor, jacuzzis y un circuito de hidroterapia,
-        todo en un entorno relajante. Nuestros terapeutas expertos se asegurarán de que cada visita sea una
-        experiencia
-        revitalizante.</p>
-      <h4>Zona de Relajacion</h4>
-      <p>Un área tranquila con jardines zen, hamacas, fuentes y un pabellón de yoga y meditación, ideal para
-        desconectar
-        y recargar energías.Es el lugar perfecto para desconectar y recargar energías rodeado de naturaleza y
-        tranquilidad.</p>
-    </div>
+  <div class="piscina">
+    <img src="https://images.fresha.com/lead-images/placeholders/spa-63.jpg?class=width-small" alt="Piscina 3">
   </div>
+  <div class="piscina">
+    <img src="https://image-tc.galaxy.tf/wijpeg-zvwqlk8yfkdsc0e9es5gqusd/pilar-50-min.jpg?width=1920" alt="Piscina 4">
+  </div>
+  <div class="piscina">
+    <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/106148685.jpg?k=64b563723c66093174c356d13a05d647b80f901f5ef34c6c8190646f6cb4735f&o=&hp=1" alt="Piscina 5">
+  </div>
+  <div class="piscina">
+    <img src="https://revistadiners.com.co/wp-content/uploads/2017/07/portada_restaurantes_hotel_1200x800.jpg" alt="Piscina 6">
+  </div>
+</div>
+
 
 </template>
 
@@ -102,17 +64,14 @@
 
 <style >
 
-body{
-  background-color: #6f4f28;
-}
 @font-face {
-    font-family: "Canela-Regular";
-    src: url("https://db.onlinewebfonts.com/t/4a24899e94d8236f671c1090cd9e068c.eot");
-    src: url("https://db.onlinewebfonts.com/t/4a24899e94d8236f671c1090cd9e068c.eot?#iefix")format("embedded-opentype"),
-    url("https://db.onlinewebfonts.com/t/4a24899e94d8236f671c1090cd9e068c.woff2")format("woff2"),
-    url("https://db.onlinewebfonts.com/t/4a24899e94d8236f671c1090cd9e068c.woff")format("woff"),
-    url("https://db.onlinewebfonts.com/t/4a24899e94d8236f671c1090cd9e068c.ttf")format("truetype"),
-    url("https://db.onlinewebfonts.com/t/4a24899e94d8236f671c1090cd9e068c.svg#Canela-Regular")format("svg");
+    font-family: "Yasashii W01 Bold";
+    src: url("https://db.onlinewebfonts.com/t/84ed805b799a7034568767668b7c9316.eot");
+    src: url("https://db.onlinewebfonts.com/t/84ed805b799a7034568767668b7c9316.eot?#iefix")format("embedded-opentype"),
+        url("https://db.onlinewebfonts.com/t/84ed805b799a7034568767668b7c9316.woff2")format("woff2"),
+        url("https://db.onlinewebfonts.com/t/84ed805b799a7034568767668b7c9316.woff")format("woff"),
+        url("https://db.onlinewebfonts.com/t/84ed805b799a7034568767668b7c9316.ttf")format("truetype"),
+        url("https://db.onlinewebfonts.com/t/84ed805b799a7034568767668b7c9316.svg#Yasashii W01 Bold")format("svg");
 }
 @font-face {
 
@@ -135,81 +94,115 @@ body{
     url("https://db.onlinewebfonts.com/t/67680f2eb947c5cbd58b40961b4a61e9.svg#Apple SD Gothic Neo Bold")format("svg");
 }
 
-.introduccion h2 {
-  z-index: 5;
-  position: absolute;
-  color: rgb(0, 0, 0);
-  text-align: center;
-  font-size: 25px;
-  font-family: "Canela-Regular";
-  margin: 0;
-  font-weight: bold; 
+.collage {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+    margin-top: 5%;
+
+  }
+
+  .piscina {
+    border-radius: 10px;
+    overflow: hidden;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    
+  }
+
+  .piscina img {
+    display: block;
+    width: 100%;
+    height: auto;
+    border-radius: 10px 10px 0 0;
+    object-fit: cover;
+  }
+
+  .piscina img:hover {
+    transform: scale(1.1);
+    transition: transform 0.3s ease;
+  }
+
+  .highlight-item {
+    margin-bottom: 10px;
+  }
+
+  .highlight-item h4 {
+    font-size: 1.2rem;
+    margin-bottom: 5px;
+  }
+
+  .highlight-item hr {
+    margin-top: 5px;
+    margin-bottom: 10px;
+    border: 0;
+    border-top: 1px solid #ccc;
+  }
+  .highlight-item img {
+    width: 50px; /* Ajusta el tamaño del icono según sea necesario */
+    height: auto;
+    margin-bottom: 10px; /* Espacio opcional entre el icono y el texto */
+  }
+
+.con {
+    position: relative;
+   
+    /* height: 60vh; */
+    background: white;
+    border-radius: 10px;
+    overflow: hidden;
+    /* box-shadow: 0 4px 8px rgba(0,0,0,0.1); */
+    display: flex;
+}
+
+.image-section {
+    flex: 9;  /* Adjust this value to make the image section larger */
+    position: relative;
+}
+
+.image-section img {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+    clip-path: polygon(0 0, 100% 0, 70% 100%, 0 100%);
+}
+
+.text-section {
+ /* Adjust this value to make the text section smaller */
+ /* padding: 10px; */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 30%;
+    height: 20%;
+
+   margin-top: 13%;
+    
+}
+
+.text-section h1 {
+    font-size: 2em;
+    margin: 0;
+    color: #000;
+    margin-left: -10%;
+  
+    font-family: "Apple SD Gothic Neo Bold";
+}
+
+.text-section p {
+    font-size: 1.2em;
+    color: #000;
+    margin-left: -10%;
+  
+}
+
+#letra{
+    position: absolute;
+    margin-left: 20%;
+    z-index: 50;
 }
 
 
-.introduccion {
-  width: 600px;
-  height: 200px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  top: 23%;
-  position: absolute;
-  left: 2%;
 
-}
-
-
-.cuadros {
-  display: flex;
-  flex-direction: row;
-  margin-left: 50%;
-  margin-top: 10%;
-  gap: 10px;
-
-}
-
-
-
-#partes {
-  flex: 1;
-  max-width: 200px;
-  z-index: 1;
-  box-sizing: border-box;
-  border-radius: 10px;
-
-}
-
-#partes img {
-  width: 100%;
-  height: auto;
-  display: block;
-  border-radius: 10px;
-
-}
-
-
-
-.contenedor {
-
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  z-index: -1;
-  /* Colocamos el z-index en -1 para asegurar que esté detrás del contenido */
-}
-
-/* Estilo para la imagen de fondo */
-.contenedor img {
-  width: 100%;
-  height: auto;
-  object-fit: cover;
-  /* Ajusta la imagen para que cubra todo el contenedor */
-  position: absolute;
-  top: 0;
-  left: 0;
-  /* filter: brightness(40%); */
-}
 
 * {
   margin: 0;
@@ -218,185 +211,42 @@ body{
 }
 
 
-
-
-#slideshow {
-  margin: 0 auto;
-  /* padding-top: 200px; */
-  /* height: 00px; */
-  width: 100%;
-  /* box-sizing: border-box; */
-  margin-top: 20%;
-
-
-}
-
-
-
-.entire-content {
-  margin: auto;
-  width: 190px;
-  perspective: 1000px;
-  /* position: relative; */
-  /* padding-top: 100px; */
-  position: absolute;
-  left: 66%;
-  top: 60%;
-
-}
-
-.content-carrousel {
-  width: 100%;
-  position: absolute;
-  float: right;
-  animation: rotar 25s infinite linear;
-  transform-style: preserve-3d;
-
-}
-
-.content-carrousel:hover {
-  animation-play-state: paused;
-  cursor: pointer;
-}
-
-.content-carrousel figure {
-  width: 100%;
-  height: 210px;
-  border: 1px solid #3b444b;
-  overflow: hidden;
-  position: absolute;
-}
-
-.content-carrousel figure:nth-child(1) {
-  transform: rotateY(0deg) translateZ(300px);
-}
-
-.content-carrousel figure:nth-child(2) {
-  transform: rotateY(40deg) translateZ(300px);
-}
-
-.content-carrousel figure:nth-child(3) {
-  transform: rotateY(80deg) translateZ(300px);
-}
-
-.content-carrousel figure:nth-child(4) {
-  transform: rotateY(120deg) translateZ(300px);
-}
-
-.content-carrousel figure:nth-child(5) {
-  transform: rotateY(160deg) translateZ(300px);
-}
-
-.content-carrousel figure:nth-child(6) {
-  transform: rotateY(200deg) translateZ(300px);
-}
-
-.content-carrousel figure:nth-child(7) {
-  transform: rotateY(240deg) translateZ(300px);
-}
-
-.content-carrousel figure:nth-child(8) {
-  transform: rotateY(280deg) translateZ(300px);
-}
-
-.content-carrousel figure:nth-child(9) {
-  transform: rotateY(320deg) translateZ(300px);
-}
-
-.content-carrousel figure:nth-child(10) {
-  transform: rotateY(360deg) translateZ(300px);
-}
-
-.shadow {
-  position: absolute;
-  box-shadow: 0px 0px 20px 0px #000;
-  border-radius: 1px;
-}
-
-.content-carrousel img {
-  image-rendering: auto;
-  transition: all 300ms;
-  width: 100%;
-  height: 100%;
-}
-
-.content-carrousel img:hover {
-  transform: scale(1.2);
-  transition: all 300ms;
-}
-
-@keyframes rotar {
-  from {
-    transform: rotateY(0deg);
-  }
-
-  to {
-    transform: rotateY(360deg);
-  }
-}
-
-
 .destacados {
-  width: 46%;
+
   /* background-color: aqua; */
   justify-content: center;
   text-align: center;
-  /* font-family: "Jost", sans-serif; */
-margin-top: 8%;
+  font-family: "Yasashii W01 Bold";
+/* position: absolute; */
+height: auto;
 
-}
+/* Ancho fijo para la sección de destacados */
 
-.destacados p {
-  font-family: "Apple SD Gothic Neo Bold";
-  font-size: 16px;
-  color: white;
-  font-weight: 300;
-  -webkit-font-smoothing: antialiased;
-}
-
-.destacados h4 {
-  font-family: "Canela-Regular";
-  line-height: 2;
-  color: rgb(0, 0, 0);
-  font-weight: bold;
-}
-
-.destacados h2 {
-  font-family: "Canela-Regular";
-  color: white;
-  
-  
-}
-
-.destacado {
-  /* background-color: aqua; */
-  justify-content: center;
-
-}
-
-.destacado p {
-  font-family: "Apple SD Gothic Neo Bold";
-  font-size: 16px;
-  color: white;
-  /* font-weight: 200; */
-  /* -webkit-font-smoothing: antialiased; */
-}
-
-.destacado h4 {
-  font-family: "Canela-Regular";
-  line-height: 2;
-  margin-left: 16%;
-  color: rgb(0, 0, 0);
-  font-weight: bold;
-}
-
-/* .segundaparte {
-  background-color: beige;
-} */
-
-
-
-
-
+ /* Ancho del bloque de destacados */
+   
+/*   
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); */
  
+
+}
+body{
+  background-color: beige;
+}
+hr{
+  width: 50%;
+  justify-content: center;
+  text-align: center;
+  margin: 20px auto;
+}
+.destacados h2{
+  font-size: 2.5rem;
+  margin-bottom: 10px;
+  line-height: 2.5;
+}
+.destacados h4{
+  font-family: "Apple SD Gothic Neo Bold";
+  line-height: 1.5;
+}
+
 </style>
